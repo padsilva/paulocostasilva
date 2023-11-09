59b7558858d8ae7dbf64f84bca5118c3f98d952a
Paulo Silva <pauloalexandreduartesilva@gmail.com>
2023-11-09 11:08:32 +0000

    nav bar

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,12 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+import {
+  DownloadIcon,
+  LaptopIcon,
+  MoonIcon,
+  SunIcon,
+} from "~/components/icons";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,8 +18,26 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <>
-      <header>
-        <nav>NAV BAR</nav>
+      <header className="px-20 py-4 border-b-[1px]">
+        <nav className="flex justify-between items-center">
+          <Link className="font-sans text-3xl" to="/">
+            Paulo Silva
+          </Link>
+          <div className="flex items-center gap-6">
+            <Link to="#education">Education</Link>
+            <Link to="#experience">Experience</Link>
+            <Link to="#skills">Skills</Link>
+            <Link to="#projects">Projects</Link>
+            <Link to="#contact">Contact</Link>
+            <div>DARK MODE TOGGLE</div>
+            <button className="flex gap-1 bg-black text-white rounded-xl px-4 py-1.5">
+              <span>CV</span>
+              <span>
+                <DownloadIcon />
+              </span>
+            </button>
+          </div>
+        </nav>
       </header>
       <main>MAIN</main>
       <footer>FOOTER</footer>
