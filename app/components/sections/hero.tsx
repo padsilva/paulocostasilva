6f3f68fec12c77415ce6911051d0faa64aca310a
Paulo Silva <pauloalexandreduartesilva@gmail.com>
2023-11-24 12:06:06 +0000

    hero section

--- app/components/sections/hero.tsx
+++ app/components/sections/hero.tsx
@@ -3,12 +3,12 @@ import { Typography } from "../typography";
 
 export function Hero() {
   return (
-    <section className="py-24 container" id="#">
-      <div className="flex flex-wrap flex-col gap-12">
-        <div className="flex flex-col items-center justify-center">
+    <section className="py-24 flex flex-col flex-wrap gap-12" id="#">
+      <div className="flex gap-12">
+        <div className="flex flex-col justify-center items-start self-stretch">
           <Avatar size="medium" name="profile" />
         </div>
-        <div className="flex flex-col items-start justify-center gap-2">
+        <div className="flex flex-col justify-center items-start self-stretch flex-[1_0_0]">
           <Typography size="h1">Hi I'm Paulo</Typography>
           <Typography size="body2">
             Graduated in Informatics Engineering, I am looking for new
