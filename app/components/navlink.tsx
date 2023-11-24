daeb9e547271101c8e8823ecf6f7fd522e082c55
Paulo Silva <pauloalexandreduartesilva@gmail.com>
2023-11-24 12:14:02 +0000

    fix dark hover effect bg color

--- app/components/navlink.tsx
+++ app/components/navlink.tsx
@@ -4,7 +4,7 @@ export function NavLink({ children, onClick, to }: LinkProps) {
   return (
     <Link
       className={
-        "capitalize md:p-0 md:hover:bg-inherit md:relative md:after:bg-black md:dark:after:bg-white md:after:absolute md:after:h-0.5 md:after:bottom-0 md:after:left-0 md:after:w-0 md:hover:after:w-full md:after:transition-all md:after:duration-500 p-6 hover:bg-slate-100 dark:hover:bg-slate-800"
+        "capitalize md:p-0 md:hover:bg-inherit md:dark:hover:bg-inherit md:relative md:after:bg-black md:dark:after:bg-white md:after:absolute md:after:h-0.5 md:after:bottom-0 md:after:left-0 md:after:w-0 md:hover:after:w-full md:after:transition-all md:after:duration-500 p-6 hover:bg-slate-100 dark:hover:bg-slate-800"
       }
       to={to}
       children={children}
