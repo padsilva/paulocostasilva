b3d8602b22b9a12164dd762effea6dd250ffd104
Paulo Silva <pauloalexandreduartesilva@gmail.com>
2024-11-27 00:01:47 +0000

    projects section

--- app/components/icons.tsx
+++ app/components/icons.tsx
@@ -196,3 +196,28 @@ export function LinkedIn({ size = 24 }: { size?: number } = {}) {
     </svg>
   );
 }
+
+export function Construction({ size = 24 }: { size?: number } = {}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect x="2" y="6" width="20" height="8" rx="1" />
+      <path d="M17 14v7" />
+      <path d="M7 14v7" />
+      <path d="M17 3v3" />
+      <path d="M7 3v3" />
+      <path d="M10 14 2.3 6.3" />
+      <path d="m14 6 7.7 7.7" />
+      <path d="m8 6 8 8" />
+    </svg>
+  );
+}
--- app/components/sections/projects.tsx
+++ app/components/sections/projects.tsx
@@ -1,7 +1,35 @@
+import { useTranslation } from "react-i18next";
+
+import { Transition } from "../transition";
+import { Construction } from "../icons";
+
 export function Projects() {
+  const { t } = useTranslation();
+
   return (
-    <section className="py-24 px-20 h-[600px]" id="projects">
-      PROJECTS
+    <section className="w-full" id="projects">
+      <div className="flex flex-col">
+        <Transition
+          className="capitalize mb-12"
+          label={t("projects")}
+          size="h2"
+        />
+
+        <div className="text-center">
+          <div className="max-w-md mx-auto flex flex-col items-center">
+            <div className="mb-6 text-black dark:text-white">
+              <Construction size={64} />
+            </div>
+
+            <Transition className="mb-4" label={t("coming_soon")} size="h3" />
+            <Transition
+              className="mb-12"
+              label={t("projects_under_construction")}
+              variant="secondary"
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
