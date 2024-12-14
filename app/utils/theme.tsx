bdf2baecc8ea2cf8b62017d335e195b83533fe9a
Paulo Silva <pauloalexandreduartesilva@gmail.com>
2024-12-14 21:37:27 +0000

    Update theme.tsx

--- app/utils/theme.tsx
+++ app/utils/theme.tsx
@@ -1,5 +1,5 @@
 import { useFetcher } from "@remix-run/react";
-import { parse } from "@conform-to/zod";
+import { parseWithZod } from "@conform-to/zod";
 import { z } from "zod";
 
 import { useRequestInfo } from "./request-info";
